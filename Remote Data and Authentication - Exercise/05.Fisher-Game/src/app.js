68e94f0d85ea6ce7fa76f31ed8b638a2a4be2c5f
YordanMilanov <dancho.galinov@gmail.com>
2023-07-31 23:04:40 +0300

    task 5 update

--- Remote Data and Authentication - Exercise/05.Fisher-Game/src/app.js
+++ Remote Data and Authentication - Exercise/05.Fisher-Game/src/app.js
@@ -1,1 +1,37 @@
-console.log('TODO:// Implement Home functionality');
+const userData = JSON.parse(localStorage.getItem('userData'));
+
+ if(userData) {
+    document.querySelector('.email span').textContent = userData.email;
+    document.getElementById('guest').style.display = 'none';
+    document.querySelector('#addForm .add').disabled = false;
+    loadData();
+ } else {
+    document.getElementById('user').style.display = "none";
+ }
+
+ // load all data
+ document.querySelector('.load').addEventListener('click', loadData);
+
+ async function loadData() {
+    const url = 'http://localhost:3030/data/catches';
+    const res = await fetch (url);
+    const data = await res.json();
+    document.getElementById('catches').replaceChildren(...data.map(createCatch))
+ }
+
+ function createCatch(data) {
+
+ }
+
+ function createElement(type, attr, ...content) {
+    const element = document.createElement(type);
+    for (const item of attr) {
+       if(item === 'class') {
+        element.classList.add(attr[item]);
+       } else if (item === 'disable') {
+        element.disabled = attr[item];
+       } else {
+        element[item] = attr[item];
+       }
+    }
+ }
--- Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,57 @@
-console.log('TODO:// Implement Register functionality');
+const registerForm = document.querySelector('form');
+registerForm.addEventListener('submit', onUserRegister)
+document.getElementById('user').style.display = "none";
+
+const url = 'http://localhost:3030/users/register';
+
+async function onUserRegister(e) {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const formData = new FormData(e.currentTarget);
+
+    //here the {email, password, rePass} should be the same as in the HTML form fields
+    const {email, password, rePass} = Object.fromEntries(formData);
+    console.log(email, password, rePass);
+
+    try {
+        //[...formData.values()] this deconstruction can be also replaced
+        //with this syntax for every field(every value of the formData)
+        // const email = formData.get('email')
+        //const password = formData.get('password')
+        //const rePass = formData.get('rePass')
+        // this is the same with [...formData.values()]
+        if([...formData.values()].some(el => el === '')) {
+            throw new Error('Input is invalid!');
+        } else if (password != rePass) {
+            throw new Error('The Password you entered do not match!');
+        }
+
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                email,
+                password,
+                rePass
+            })
+        })
+
+        if(!res.ok) {
+            const error = await res.json();
+            throw new Error(error.message);
+        }
+        const data = await res.json()
+        console.log(data);
+        const user = {
+            email: data.email,
+            id: data._id,
+            token: data.accessToken
+        }
+
+        localStorage.setItem('userData', JSON.stringify(user));
+        window.location = './index.html';
+    } catch(error) {
+        document.querySelector('form').reset();
+        alert(error.message)
+    }
+}
