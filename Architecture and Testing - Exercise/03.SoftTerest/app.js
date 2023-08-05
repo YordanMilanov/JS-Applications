e30ce92b4f5cc5b5a96d4c9acfaa2f48ea601ece
YordanMilanov <dancho.galinov@gmail.com>
2023-08-05 23:28:24 +0300

    to be finished

--- Architecture and Testing - Exercise/03.SoftTerest/app.js
+++ Architecture and Testing - Exercise/03.SoftTerest/app.js
@@ -1,1 +1,38 @@
+import { showCatalog } from "./views/catalog.js";
+import { showHomeView } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+
 document.getElementById('defSection').remove();
+document.querySelector('nav').addEventListener('click', onNavigateClick)
+
+const links = {
+    "/" : showHomeView,
+    '/' : catalogView,
+    '/login': showLogin,
+    'register': showRegister,
+    'create': showCreate,
+    'details' : showDetails,
+    //todo logout
+}
+
+const main = document.getElementById('mainView')
+
+const context = {
+    showSection,
+}
+
+window.showHomeView = showHomeView(context);
+
+function showSection(section) {
+    main.replaceChildren(section);
+}
+
+function onNavigateClick(event) {
+
+    event.preventDefault();
+
+    console.log(event.target);
+}
